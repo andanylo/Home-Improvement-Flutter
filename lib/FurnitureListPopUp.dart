ee938e85ae4e63488799eb5417668524c03c3d03
andanylo <andanylo@gmail.com>
2022-09-26 13:11:52 -0400

    Communication between Unity and edit switch

---
+++ lib/FurnitureListPopUp.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class FurnitureListPopUp extends StatefulWidget {
+  FurnitureListPopUp({super.key});
+
+  _FurnitureListPopUp createState() => _FurnitureListPopUp();
+}
+
+class _FurnitureListPopUp extends State<FurnitureListPopUp> {
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,8 @@ Future<void> main() async {
 
   runApp(MaterialApp(
       home: (FirebaseAuth.instance.currentUser != null)
-          ? LoginPage()
-          : UnityDemoScreen()));
+          ? UnityDemoScreen()
+          : UnityDemoScreen())); //(FirebaseAuth.instance.currentUser != null) ? LoginPage() : UnityDemoScreen())
 }
 
 class UnityDemoScreen extends StatefulWidget {
@@ -30,32 +30,119 @@ class _UnityDemoScreenState extends State<UnityDemoScreen> {
       GlobalKey<ScaffoldState>();
   late UnityWidgetController _unityWidgetController;
 
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _unityWidgetController.dispose();
+    super.dispose();
+  }
+
+  var isEditing = false;
+
+  Widget? unityWidget;
+
   Widget build(BuildContext context) {
     return Scaffold(
-      key: _scaffoldKey,
-      appBar: AppBar(
-        title: const Text("Home improvement"),
-        automaticallyImplyLeading: false,
-      ),
-      body: SafeArea(
-        bottom: false,
-        child: WillPopScope(
-          onWillPop: () {
-            return Future<bool>.value(true);
-            // Pop the category page if Android back button is pressed.
-          },
-          child: Container(
-            child: UnityWidget(
-              onUnityCreated: onUnityCreated,
-            ),
-          ),
+        appBar: AppBar(
+          title: const Text("Home improvement"),
+          automaticallyImplyLeading: false,
+          leading: GestureDetector(
+              onTap: () {
+                setIsEditing();
+              },
+              child: this.isEditing
+                  ? const Center(
+                      child: Text("  Cancel",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontWeight: FontWeight.w700, fontSize: 15)),
+                    )
+                  : const Icon(Icons.edit)),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 20),
+              child: GestureDetector(
+                onTap: () {},
+                child: const Icon(Icons.account_circle_outlined),
+              ),
+            )
+          ],
         ),
-      ),
-    );
+        body: Stack(
+          children:
+              this.isEditing ? setUpEditingWindow() : disableEditingWindow(),
+        ));
+  }
+
+  //Get unity widget and crete if needed
+  Widget returnUnityWidget() {
+    if (this.unityWidget == null) {
+      this.unityWidget = UnityWidget(
+        onUnityCreated: onUnityCreated,
+        onUnityMessage: onUnityMessage,
+        onUnitySceneLoaded: onUnitySceneLoaded,
+        useAndroidViewSurface: true,
+        fullscreen: false,
+      );
+    }
+    return this.unityWidget!;
   }
 
   // Callback that connects the created controller to the unity controller
   void onUnityCreated(controller) {
     this._unityWidgetController = controller;
+  }
+
+  void onUnityMessage(message) {
+    print('Received message from unity: ${message.toString()}');
+  }
+
+  //Set is editing, add 'Add' button
+  void setIsEditing() {
+    bool editing = !this.isEditing;
+    _unityWidgetController.postMessage(
+        'EditSwitcher', 'changeEditingStatus', '$editing');
+
+    setState(() {
+      this.isEditing = !this.isEditing;
+    });
+  }
+
+  //Get widgets
+  List<Widget> setUpEditingWindow() {
+    Widget unity = returnUnityWidget();
+    return [
+      unity,
+      Align(
+        alignment: Alignment.bottomCenter,
+        child: SafeArea(
+          bottom: true,
+          child: Container(
+              margin: EdgeInsets.only(bottom: 30),
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  shape: CircleBorder(),
+                  padding: EdgeInsets.all(24),
+                ),
+                child: Icon(Icons.add),
+              )),
+        ),
+      )
+    ];
+  }
+
+  List<Widget> disableEditingWindow() {
+    Widget unity = returnUnityWidget();
+    return [unity];
+  }
+
+  void onUnitySceneLoaded(SceneLoaded? scene) {
+    print('Received scene loaded from unity: ${scene?.name}');
+    print('Received scene loaded from unity buildIndex: ${scene?.buildIndex}');
   }
 }
